b585d8caa725078adf16a8a59265485bf9f3c071
abid7 <abidshahriar7@gmail.com>
2021-01-19 19:32:38 +0600

    adding register function

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,13 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secret = process.env.JWT_SECRET;
+
+import userModel from '../models/userModel.js';
+
 export const signIn = (req, res) => {
 	console.log(req.body.password);
 	if (req.body.password !== '12345567') {
@@ -5,4 +15,25 @@ export const signIn = (req, res) => {
 	}
 
 	res.json({ user: req.body });
+};
+export const signUp = async (req, res) => {
+	const { firstName, lastName, email, password } = req.body;
+
+	try {
+		const oldUser = await userModel.findOne({ email });
+
+		if (oldUser) return res.status(400).json({ message: 'User Already Exist' });
+
+		const hashedPass = await bcrypt.hash(password, 12);
+
+		const newUser = { ...req.body, password: hashedPass };
+
+		await userModel.create(newUser);
+
+		const token = jwt.sign({ email, firstName, lastName }, secret, { expiresIn: '1w' });
+
+		res.status(201).json(token);
+	} catch (error) {
+		console.log(error);
+	}
 };
---
+++ server/models/userModel.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true }
+});
+
+export default mongoose.model('User', userSchema);
