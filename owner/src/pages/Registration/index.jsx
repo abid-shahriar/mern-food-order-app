6a8c7adb4e1a6063b38bab39103c5f3eba1db4eb
abid7 <abidshahriar7@gmail.com>
2021-02-18 01:17:02 +0600

    layout changes

--- owner/src/pages/Registration/index.jsx
+++ owner/src/pages/Registration/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+
 import RegistrationForm from './RegistrationForm';
 
 const RegistrationFormPage = () => {
 	return (
-		<div>
-			<RegistrationForm />
-		</div>
+		<Grid padded width='16' centered style={{ minHeight: 'calc(100vh - 72px)' }}>
+			<Grid.Row style={{ alignItems: 'center' }}>
+				<RegistrationForm />
+			</Grid.Row>
+		</Grid>
 	);
 };
 
